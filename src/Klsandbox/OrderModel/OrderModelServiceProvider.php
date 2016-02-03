7e7fa4512db3da26a61a783af1de96c4c68cacd5
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-02-03 09:43:27 +0800

    init

---
+++ src/Klsandbox/OrderModel/OrderModelServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Klsandbox\OrderModel;
+
+use Illuminate\Support\ServiceProvider;
+
+class OrderModelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
