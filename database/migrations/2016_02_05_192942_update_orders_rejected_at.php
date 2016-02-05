7d087b386efe2afe9e63a64aec05c7e164b2b4d9
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-02-05 19:38:16 +0800

    update to store reject and accept details

---
+++ database/migrations/2016_02_05_192942_update_orders_rejected_at.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOrdersRejectedAt extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('orders', function(Blueprint $table)
+		{
+			$table->timestamp('rejected_at')->nullable();
+			$table->integer('rejected_by_id')->unsigned()->nullable();
+			$table->foreign('rejected_by_id')->references('id')->on('users');
+
+			$table->integer('accepted_by_id')->unsigned()->nullable();
+			$table->foreign('accepted_by_id')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('orders', function(Blueprint $table)
+		{
+			$table->dropColumn('rejected_at');
+			$table->dropForeign('orders_rejected_by_id_foreign');
+			$table->dropColumn('rejected_by_id');
+			$table->dropForeign('orders_accepted_by_id_foreign');
+			$table->dropColumn('accepted_by_id');
+		});
+	}
+}
