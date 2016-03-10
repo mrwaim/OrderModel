b294fa4242c78da9ef449132a4041eb9f9b6c03a
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-10 17:14:25 +0800

    import Product Code

--- src/Klsandbox/OrderModel/Models/Product.php
+++ src/Klsandbox/OrderModel/Models/Product.php
@@ -3,6 +3,8 @@
 namespace Klsandbox\OrderModel\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Klsandbox\RoleModel\Role;
+use Klsandbox\SiteModel\Site;
 
 /**
  * Klsandbox\OrderModel\Models\Product
@@ -39,4 +41,55 @@ class Product extends Model
         return Product::forSite()->where('name', '=', 'Restock')->first()->id;
     }
 
+    public static function OtherPricingId()
+    {
+        return self::where('name', 'Other')
+            ->select('product_pricings.id as product_pricing_id')
+            ->leftJoin('product_pricings', 'products.id', '=', 'product_pricings.product_id')
+            ->first()->product_pricing_id;
+    }
+
+    public static function getList()
+    {
+        $q = self::where('products.site_id', '=', Site::id())
+            ->where('products.is_available', true);
+
+        $q = $q->select(
+            'products.id',
+            'products.name',
+            'products.description',
+            'product_pricings.price',
+            'product_pricings.id as product_pricing_id'
+        )
+            ->leftJoin('product_pricings', 'products.id', '=', 'product_pricings.product_id');
+
+        return $q->get();
+    }
+
+    public function createNew($input)
+    {
+        $product = new Product();
+
+        $product->name = $input['name'];
+        $product->description = $input['description'];
+        $product->is_available = true;
+        $product->hidden_from_ordering = false;
+        $product->site_id = Site::id();
+        $product->save();
+
+        $product_price = new ProductPricing();
+
+        $product_price->role_id = Role::Stockist()->id;
+        $product_price->product_id = $product->id;
+        $product_price->price = $input['price'];
+        $product_price->site_id = Site::id();
+        $product_price->save();
+    }
+
+    public function setUnavailable($id)
+    {
+        $product = Product::find($id);
+        $product->is_available = false;
+        $product->save();
+    }
 }
