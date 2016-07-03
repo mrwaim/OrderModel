85f119b0bdee552f0e98956efcad1f395a7955ea
Klsandbox <ibrahim@klsandbox.com>
2016-07-03 15:01:30 +0800

    trait

--- src/Klsandbox/OrderModel/Models/Order.php
+++ src/Klsandbox/OrderModel/Models/Order.php
@@ -64,6 +64,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use \Klsandbox\SiteModel\SiteExtensions;
+    use App\Models\Traits\OrganizationTrait;
 
     public static function boot()
     {
